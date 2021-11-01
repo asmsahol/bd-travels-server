ea48c2e2afba7a59c33fa9d9fc64f7e866843a45
asmsahol <sahol.bin.mijan@gmail.com>
2021-11-01 11:47:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/** @format */
+
+const express = require("express");
+const { MongoClient, Collection } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewere
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.apmnd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("bd_travel");
+    const servicesCollection = database.collection("services");
+
+    // Get Products API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("The server in running.");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
